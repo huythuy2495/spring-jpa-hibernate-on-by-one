cb31a3ab6ec119f3486e2f70498b4871c0ea063f
huythuy2495 <31379372+huythuy2495@users.noreply.github.com>
2017-08-31 15:29:42 +0700

    Add files via upload

---
+++ src/main/java/com/techprimers/jpa/springjpahibernateexample/SpringJpaHibernateExampleApplication.java
@@ -0,0 +1,15 @@
+package com.techprimers.jpa.springjpahibernateexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories(basePackages = "com.techprimers.jpa.springjpahibernateexample.repository")
+@SpringBootApplication
+public class SpringJpaHibernateExampleApplication {
+
+	public static void main(String[] args) {
+		System.getProperties().put("server.port",80);
+		SpringApplication.run(SpringJpaHibernateExampleApplication.class, args);
+	}
+}
